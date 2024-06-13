42b6fe953a9e352d3b1b58d2534fdec7f98f5d8f
OCTAGON-EVENTS <shethzor+octagon@gmail.com>
2024-06-13 19:05:42 +0200

    Added generator

---
+++ EventText/eventText.js
@@ -0,0 +1,53 @@
+const eventText = [
+    { 
+        "de" : {
+            "discord" : `
+            @Event Update︱🔔
+            # OCTAGON Event
+            > Von \`<START_TIME> Uhr abends\` bis \`<END_TIME> Uhr morgens\`
+            ## Event Quest
+            > **<QUEST_TITLE>**
+            > <QUEST_TEXT>
+            
+            _Trink verantwortungsbewusst. Deine Gesundheit ist wichtig!_
+            _<CHEERS_TEXT>!_
+            ||<CHEERS_LANGUAGE>: <CHEERS_PRONOUNCIATION>||
+            [ˢᵗᵃʳᵗᵉ ᵛʳᶜʰᵃᵗ ᵃᵘᶠ ˢᵗᵉᵃᵐ](<https://shethzor.tv/?launch_steam=438100>)
+            `,
+            "vrchat" : `
+            <QUEST_TITLE>
+
+            <QUEST_TEXT>
+
+            <CHEERS_TEXT> (<CHEERS_LANGUAGE>)
+            
+            Aussprache:
+            <CHEERS_CUSTOM>
+            `
+        },
+        "en": {
+            "discord": `
+            @Event Update︱🔔
+            # OCTAGON Event
+            > From \`<START_TIME> pm\` till \`<END_TIME> am\`
+            ## Event Quest
+            > **<QUEST_TITLE>**
+            > <QUEST_TEXT>
+            
+            _Drink responsibly. Your health is important!_
+            _<CHEERS_TEXT>!_
+            ||<CHEERS_LANGUAGE>: <CHEERS_PRONOUNCIATION>||
+            [ˢᵗᵃʳᵗ ᵛʳᶜʰᵃᵗ ᵒⁿ ˢᵗᵉᵃᵐ](<https://shethzor.tv/?launch_steam=438100>)`,
+            "vrchat": `
+            <QUEST_TITLE>
+
+            <QUEST_TEXT>
+
+            <CHEERS_TEXT> (<CHEERS_LANGUAGE>)
+
+            Pronounciation:
+            <CHEERS_CUSTOM>
+            `
+        }
+    }
+]
---
+++ EventText/quests.js
@@ -0,0 +1,133 @@
+const quests = [
+    { 
+        "de" : {
+            "title" : "2 Dumme 1 Gedanke",
+            "text" : "Stelle jemandem eine \"Würdest du eher...\" Frage.\nWenn eure Antworten übereinstimmen, genießt gemeinsam einen Shot, um die Verbundenheit zu feiern.\nWenn nicht, nehmt drei Schlücke, um die Vielfalt der Meinungen zu würdigen!"
+        },
+        "en": {
+            "title": "2 Idiots 1 Thought",
+            "text": "Pose a 'Would you rather...' question to someone.\nIf your answers match, enjoy a shot together to celebrate your connection.\nIf not, take three sips to appreciate the diversity of opinions!"
+        }
+    },
+
+    {
+        "de" : {
+            "title" : "Frischfleisch",
+            "text" : "Suche einen Spieler, den du nicht kennst oder an den du dich nicht erinnerst, und starte das Gespräch mit \"Und, wer bist du?\".\nWenn ihr ins Gespräch kommt, hebt die Stimmung mit einem gemeinsamen Shot!"
+        },
+        "en" : {
+            "title" : "Fresh Meat",
+            "text" : "Find a player you don't know or don't remember and start a conversation with 'So, who are you?'.\nIf you hit it off, lift the mood with a shared shot!"
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Tag Team",
+            "text" : "Finde dir einen Partner für den Rest des Events.\nWenn du trinkst, trinkt dein Partner und umgekehrt.\nZusammen seid ihr ein unschlagbares Team, das die Freuden des Erfolgs gemeinsam genießt!"
+        },
+        "en" : {
+            "title" : "Tag Team",
+            "text" : "Find yourself a partner for the rest of the event.\nWhen you drink, your partner drinks, and vice versa.\nTogether you're an unbeatable team, enjoying the joys of success together!"
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Spiegel",
+            "text" : "Suche dir einen Partner und tauscht für den Rest des Events eure Shots."
+        },
+        "en" : {
+            "title" : "Mirror",
+            "text" : "Find yourself a partner and swap shots for the remainder of the event."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Der Anthropomorph",
+            "text" : "Wenn jemand einen Avatar mit tierischen Eigenschaften trägt, geh zu ihm hin und fordere ihn mit einem großen Schluck heraus."
+        },
+        "en" : {
+            "title" : "The Anthropomorph",
+            "text" : "If someone wears an avatar with animal characteristics, approach them and challenge them with a big gulp."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Sharing is caring",
+            "text" : "Du darfst die Anzahl deiner Shots jederzeit mit einer beliebigen Person teilen.\nBeide trinken dann jeweils ihre Anzahl an Shots + 1."
+        },
+        "en" : {
+            "title" : "Sharing is caring",
+            "text" : "You can share the number of your shots with anyone at any time.\nBoth then drink their number of shots + 1."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "n + 1",
+            "text" : "Wenn jemand trinken muss, kann man besagter Person mit den Worten \"PLUS EINS\" einen weiteren Shot schenken.\nDiese Regel ist mit Ausnahme von +1 Shots auf alle zu trinkenden Shots anwendbar."
+        },
+        "en" : {
+            "title" : "n + 1",
+            "text" : "When someone has to drink, you can give that person another shot by saying 'PLUS ONE'.\nThis rule applies, except for +1 shots on all shots to be taken."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Iss Seife",
+            "text" : "Wer ein Schimpfwort benutzt, darf von anderen zu einem Shot aufgefordert werden."
+        },
+        "en" : {
+            "title" : "Eat Soap",
+            "text" : "Anyone who uses a swear word may be challenged by others to take a shot."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "T",
+            "text" : "Wenn du jemanden in der T-Pose siehst, spendiere ihm einen Shot!"
+        },
+        "en" : {
+            "title" : "T",
+            "text" : "If you see someone in a T-pose, buy them a shot!"
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "GAS GAS GAS!",
+            "text" : "Trinke für jeden Rülpser einer anderen Person (mit 10 Minuten Abkühlzeit pro Person)."
+        },
+        "en" : {
+            "title" : "GAS GAS GAS!",
+            "text" : "Drink for every burp from another person (with a 10-minute cooldown per person)."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Hast du ein Leben? Nein, ich habe VRChat!",
+            "text" : "Geh zu einer Person, frag sie, ob sie ein Leben hat, und rate anschließend, wie viele Stunden sie in VRChat verbracht hat.\nLiegst du richtig, muss dein Gegenüber trinken, andernfalls trinkst du."
+        },
+        "en" : {
+            "title" : "Do you have a life? No, I have VRChat!",
+            "text" : "Approach someone, ask them if they have a life, and then guess how many hours they've spent in VRChat.\nIf you guess correctly, the other person drinks; otherwise, you drink."
+        }
+    },
+
+    { 
+        "de" : {
+            "title" : "Hörst du Rick?",
+            "text" : "Sobald du den Song \"Never Gonna Give You Up\" von Rick Astley hörst, teile fünf Shots aus."
+        },
+        "en" : {
+            "title" : "Can you hear Rick?",
+            "text" : "When you hear the song 'Never Gonna Give You Up' by Rick Astley, give out five shots."
+        }
+    }
+]
