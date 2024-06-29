d2e0e0f84bb1801a5a763704ea059d4b42ef62b9
OCTAGON-EVENTS <shethzor+octagon@gmail.com>
2024-06-29 19:45:27 +0200

    Added Links, Display right Timezone

--- EventText/eventText.js
+++ EventText/eventText.js
@@ -4,7 +4,7 @@ const eventText = [
             "discord" : `
             # OCTAGON Event
             > Von \`<START_TIME> Uhr abends\` bis \`<END_TIME> Uhr morgens\`
-            > _Zeiten sind in UTC+1_
+            > _Zeiten sind in UTC+2_
             ## Event Quest
             > **<QUEST_TITLE>**
             > <QUEST_TEXT>
@@ -14,7 +14,9 @@ const eventText = [
             ||<CHEERS_LANGUAGE>: <CHEERS_PRONOUNCIATION>||
             [ˢᵗᵃʳᵗᵉ ᵛʳᶜʰᵃᵗ ᵃᵘᶠ ˢᵗᵉᵃᵐ](<https://shethzor.tv/?launch_steam=438100>)
             
-            :information_source: _Du kannst die Rolle <@&1233131412241121400> jederzeit in <id:customize> entfernen_`,
+            :information_source: _Du kannst die Rolle <@&1233131412241121400> jederzeit in <id:customize> entfernen_
+
+            :goggles: [VRChat](<https://vrc.group/OCTO.8998>) | :pig: [Discord](<https://discord.gg/OCTAGON>) | :camera: [Instagram](<https://instagram.com/OCTAGON.VRC>)`,
             "vrchat" : `
             <QUEST_TITLE>
 
@@ -30,7 +32,7 @@ const eventText = [
             "discord": `
             # OCTAGON Event
             > From \`<START_TIME> pm\` till \`<END_TIME> am\`
-            > _Times are in UTC+1_
+            > _Times are in UTC+2_
             ## Event Quest
             > **<QUEST_TITLE>**
             > <QUEST_TEXT>
@@ -40,7 +42,9 @@ const eventText = [
             ||<CHEERS_LANGUAGE>: <CHEERS_PRONOUNCIATION>||
             [ˢᵗᵃʳᵗ ᵛʳᶜʰᵃᵗ ᵒⁿ ˢᵗᵉᵃᵐ](<https://shethzor.tv/?launch_steam=438100>)
             
-            :information_source: _You can remove the <@&1233131412241121400> role in <id:customize> at all times_`,
+            :information_source: _You can remove the <@&1233131412241121400> role in <id:customize> at all times_
+
+            :goggles: [VRChat](<https://vrc.group/OCTO.8998>) | :pig: [Discord](<https://discord.gg/OCTAGON>) | :camera: [Instagram](<https://instagram.com/OCTAGON.VRC>)`,
             "vrchat": `
             <QUEST_TITLE>
 
