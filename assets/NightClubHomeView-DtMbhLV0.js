6a94515a739da65c600f9737b6a0c4515aff79b7
OCTAGON-EVENTS <info.octagon.events@gmail.com>
2024-12-25 13:29:31 +0100

    Removed app padding; Removed hash redirect

--- assets/NightClubHomeView-DtMbhLV0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-Nz5wxHXt.js";const t={};function n(r,a){return o(),c("main",null,"Night Club Home")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/RaveHomeView-CHSzGVLa.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-CgZ0d_lP.js";const n={};function a(r,t){return o(),c("main",null,"Rave Home")}const _=e(n,[["render",a]]);export{_ as default};
