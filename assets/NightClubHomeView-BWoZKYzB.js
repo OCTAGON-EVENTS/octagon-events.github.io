31ef08361e3c39ecf66db944c1fc3f59ad855d73
OCTAGON-EVENTS <info.octagon.events@gmail.com>
2024-12-28 22:53:37 +0100

    Add links to Home

--- assets/NightClubHomeView-BWoZKYzB.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-D8_t258I.js";const t={};function n(r,a){return o(),c("main",null,"Night Club Home")}const _=e(t,[["render",n]]);export{_ as default};
--- assets/RaveHomeView-B206exrz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-D8_t258I.js";const n={};function a(r,t){return o(),c("main",null,"Rave Home")}const _=e(n,[["render",a]]);export{_ as default};
