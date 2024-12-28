d7e6bea0b1bc194dc37e3111104168ab4ee48ace
OCTAGON-EVENTS <info.octagon.events@gmail.com>
2024-12-28 14:36:32 +0100

    Add new Team member; Try image fix

---
+++ assets/GeneralHomeView-DWqkuLwe.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,o as c}from"./index-lBdz6T5z.js";const o={};function r(a,t){return c(),n("main",null,"General Home")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/LegalNotice-PjxkXeuV.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-lBdz6T5z.js";const c={};function t(r,o){return" Legal Notice "}const n=e(c,[["render",t]]);export{n as default};
